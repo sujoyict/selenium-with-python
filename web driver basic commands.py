4bd24226e8f7e73704d9dcb340b80a7765ff8d57
Sujoy Sarker <sujoyict@gmail.com>
2021-08-10 14:34:59 +0600

    web driver navigation

--- web driver basic commands.py
+++ web driver basic commands.py
@@ -4,18 +4,16 @@ import time
 
 driver = webdriver.Chrome(executable_path="C:\drivers\crome driver\chromedriver.exe")
 
-driver.get("http://www.seleniumframework.com/python-course/")
+driver.get("http://demo.automationtesting.in/Windows.html")
 
 print(driver.title)
 
 print(driver.current_url)
 
-#print(driver.page_source)
-
-driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[2]/div/div/h2[1]/a/input").click()
+driver.find_element_by_xpath("//*[@id='Tabbed']/a/button").click()
 
 time.sleep(5)
 
-driver.close()
+#driver.close()
 
-fff
+driver.quit()
